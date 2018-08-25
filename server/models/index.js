6260ac9c27f187707e1e7462f1264e2a24e2f937
davydhong <davydhong@gmail.com>
2018-08-24 22:47:04 -0700

    work in progress

--- server/models/index.js
+++ server/models/index.js
@@ -1,15 +1,29 @@
 var db = require('../db');
-
+var headers = {
+  'access-control-allow-origin': '*',
+  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'access-control-allow-headers': 'content-type, accept',
+  'access-control-max-age': 10, // Seconds.
+  'Content-Type': 'application/json'
+};
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function(response, data, statusCode) {
+      db.query('SELECT~~~', () => {
+        response.writeHead(statusCode, headers);
+        response.end('query output');
+      });
+    }, // a function which produces all the messages
+    post: function() {} // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function(response, data, statusCode) {
+      // statusCode = statusCode || 200;
+      // response.writeHead(statusCode, headers);
+      // response.end('get users');
+    },
+    post: function() {}
   }
 };
-
--- server/schema.sql
+++ server/schema.sql
@@ -1,17 +1,78 @@
 CREATE DATABASE chat;
-
 USE chat;
-
-CREATE TABLE messages (
-  /* Describe your table here.*/
-);
-
+-- CREATE TABLE messages (
+--   /* Describe your table here.*/
+-- );
 /* Create other tables and define schemas for them here! */
-
-
-
-
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
+DROP DATABASE IF EXISTS chat;
+CREATE DATABASE chat;
+USE chat;
+-- CREATE TABLE messages (
+--   /* Describe your table here.*/
+-- );
+/* Create other tables and define schemas for them here! */
+/*  Execute this file from the command line by typing:
+ *    mysql -u root < server/schema.sql
+  *    mysql -u student -p < server/schema.sql
+ *  to create the database and the tables.*/
+-- ---
+-- Globals
+-- ---
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+DROP TABLE IF EXISTS Users;
+create table Users
+(
+  id INT NOT NULL
+  AUTO_INCREMENT,
+  UserID VARCHAR
+  (100) NOT NULL,
+  PRIMARY KEY
+  (id)
+);
+  DROP TABLE IF EXISTS Messages;
+  CREATE TABLE Messages
+  (
+    id INT NOT NULL
+    AUTO_INCREMENT,
+  id_User INT,
+  Text VARCHAR
+    (400) NULL DEFAULT NULL,
+  id_Rooms INT,
+  PRIMARY KEY
+    (id)
+);
+    DROP TABLE IF EXISTS Rooms;
+    CREATE TABLE Rooms
+    (
+      id INT NOT NULL
+      AUTO_INCREMENT,
+  RoomName VARCHAR
+      (100),
+  PRIMARY KEY
+      (id)
+);
+      ALTER TABLE Messages ADD FOREIGN KEY (id_User) REFERENCES Users (id);
+      ALTER TABLE Messages ADD FOREIGN KEY (id_Rooms) REFERENCES Rooms (id);
 
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE Users ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE Messages ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE Rooms ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO Users (id,UserID) VALUES
+-- ('','');
+-- INSERT INTO Messages (id,id_User,Text,id_Rooms) VALUES
+-- ('','','','');
+-- INSERT INTO Rooms (id,RoomName) VALUES
+-- ('','');
